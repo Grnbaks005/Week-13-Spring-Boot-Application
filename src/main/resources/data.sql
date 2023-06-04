4c37e4d7648ba11bc326ec81ad8230372e6bbdce
Grnbaks005 <integruss00@yahoo.com>
2023-06-04 10:45:29 -0700

    Week 13 Pet Store Spring Boot App

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO pet_store (pet_store) VALUES('Pets R Us');
+INSERT INTO pet_store (pet_store) VALUES('Anaheim');
+INSERT INTO pet_store (pet_store) VALUES('California');
+INSERT INTO pet_store (pet_store) VALUES('562-867-5309');
